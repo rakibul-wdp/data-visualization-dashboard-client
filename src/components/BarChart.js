8a774d22596e08a068b218e240673f90b6ca61a7
rakibul-wdp <rakibul.wdp@gmail.com>
2022-07-27 18:39:40 +0600

    add bar chart in dashboard

---
+++ src/components/BarChart.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import useCharts from '../hooks/useCharts';
+
+const BarChart = () => {
+  const charts = useCharts();
+
+  return (
+    <div className='mt-16 mx-16 border-4 border-primary rounded'>
+      <h2 className='font-bold text-xl m-8 text-accent'>Intensity, Relevance, and Likelihood related to Country</h2>
+      <Bar
+        data={{
+          labels: charts.map((data) => data.country),
+          datasets: [
+            {
+              label: 'Intensity',
+              data: charts.map((data) => data.intensity),
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+            {
+              label: 'Relevance',
+              data: charts.map((data) => data.relevance),
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+            {
+              label: 'Likelihood',
+              data: charts.map((data) => data.likelihood),
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        }}
+        options={
+          {
+            // maintainAspectRatio: false,
+          }
+        }
+      />
+    </div>
+  );
+};
+
+export default BarChart;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import useCharts from '../hooks/useCharts';
+import BarChart from './BarChart';
 
 const Dashboard = () => {
-  const [charts] = useCharts();
-  console.log(charts);
   return (
     <div>
-      <h2>This is dashboard here.</h2>
+      <BarChart />
     </div>
   );
 };
